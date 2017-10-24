ff3beac6db8b0e4a0ef2fc3f618f385abeae8450
Michael Anderson <suzeyang2@hotmail.com>
2017-10-24 16:54:18 -0400

    Moved Mike's imports out of main

--- app/src/main/java/com/access/accessonandroid/MainActivity.java
+++ app/src/main/java/com/access/accessonandroid/MainActivity.java
@@ -2,54 +2,8 @@ package com.access.accessonandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
-
-/* Mike's Imports */
-import android.util.Log;
-
-import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-import com.amazonaws.services.rekognition.AmazonRekognition;
-import com.amazonaws.services.rekognition.AmazonRekognitionClient;
-//import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.auth.AWSCredentials;
-//import com.amazonaws.auth.AWSStaticCredentialsProvider;
-//import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Regions;
-//import com.amazonaws.services.rekognition.model.AmazonRekognitionException;
-import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
-import com.amazonaws.services.rekognition.model.DetectLabelsResult;
-import com.amazonaws.services.rekognition.model.Image;
-import com.amazonaws.services.rekognition.model.Label;
-import com.amazonaws.services.rekognition.model.S3Object;
-import java.util.List;
-
-/* Detect Face Staff */
-//import com.amazonaws.services.rekognition.model.Attribute;
-//import com.amazonaws.services.rekognition.model.DetectFacesRequest;
-//import com.amazonaws.services.rekognition.model.DetectFacesResult;
-//import com.amazonaws.services.rekognition.model.FaceDetail;
-//import com.amazonaws.services.rekognition.model.AgeRange;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-/* End of Detect Face Staff */
-
-/* Compare Face Staff */
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-import com.amazonaws.util.IOUtils;
-import com.amazonaws.services.rekognition.model.CompareFacesMatch;
-import com.amazonaws.services.rekognition.model.CompareFacesRequest;
-import com.amazonaws.services.rekognition.model.CompareFacesResult;
-import com.amazonaws.services.rekognition.model.ComparedFace;
-import com.amazonaws.services.rekognition.model.BoundingBox;
-
 import android.content.Intent;
 
-/* End of Compare Face Staff */
-/* End of Mike's Imports*/
 
 public class MainActivity extends AppCompatActivity {
 
