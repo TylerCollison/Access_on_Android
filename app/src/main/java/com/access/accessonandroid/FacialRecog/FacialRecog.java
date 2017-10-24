9b3044f2a774c1e9a078ce3151945c7a0c5eb0d0
Michael Anderson <suzeyang2@hotmail.com>
2017-10-24 19:08:41 -0400

    Moved Mike's imports out of main

--- app/src/main/java/com/access/accessonandroid/FacialRecog/FacialRecog.java
+++ app/src/main/java/com/access/accessonandroid/FacialRecog/FacialRecog.java
@@ -20,6 +20,15 @@ public interface FacialRecog {
 
 
     /**
+     * Create a new thread before asking AWS Rekognition to comparefaces. Do not use unless absolutely necessary.
+     * @param imageA
+     * @param imageB
+     * @return
+     */
+    public boolean compareFacesThreaded(final Image imageA, final Image imageB);
+
+
+    /**
      * NOT YET IMPLEMENTED
      * @param s3FileName_1
      * @param s3BucketName_1
