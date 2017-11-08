dc628443375c0d2bdb3b2eac1748145b00110822
maehutch <hutchison.190@osu.edu>
2017-11-08 14:42:02 -0500

    test

--- app/src/main/java/com/access/accessonandroid/CameraPreview.java
+++ app/src/main/java/com/access/accessonandroid/CameraPreview.java
@@ -2,20 +2,37 @@ package com.access.accessonandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.hardware.camera2;
 
 public class CameraPreview extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //maybe this will all be abstracted away from here later
-//        setContentView(R.layout.activity_main);
-//        startActivity(new Intent(MainActivity.this, Registration.class));
 
-        //code to run authenticator class which will authenticate the user
-//        Authenticator authenticator = new Authenticator();
-//        authenticator.auth(this);
 
+    }
+
+    private boolean safeCameraOpen(int id) {
+        boolean qOpened = false;
+
+        try {
+            releaseCameraAndPreview();
+            mCamera = camera2.open(id);
+            qOpened = (mCamera != null);
+        } catch (Exception e) {
+            Log.e(getString(R.string.app_name), "failed to open Camera");
+            e.printStackTrace();
+        }
+
+        return qOpened;
+    }
+
+    private void releaseCameraAndPreview() {
+        mPreview.setCamera(null);
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
     }
 }
