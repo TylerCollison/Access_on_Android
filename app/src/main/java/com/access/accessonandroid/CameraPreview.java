feb9c87c40836cfcdda572218eeb992a6a6ca1d9
maehutch <hutchison.190@osu.edu>
2017-11-08 14:35:08 -0500

    test

---
+++ app/src/main/java/com/access/accessonandroid/CameraPreview.java
@@ -0,0 +1,21 @@
+package com.access.accessonandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class CameraPreview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //maybe this will all be abstracted away from here later
+//        setContentView(R.layout.activity_main);
+//        startActivity(new Intent(MainActivity.this, Registration.class));
+
+        //code to run authenticator class which will authenticate the user
+//        Authenticator authenticator = new Authenticator();
+//        authenticator.auth(this);
+
+    }
+}
