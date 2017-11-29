01a6b97c8428833cef2a45f8fe8198fa5fc4af84
Zeyang Su <michael2488s@users.noreply.github.com>
2017-11-29 17:36:30 -0500

    Update FacialRecogRunnable.java

--- app/src/main/java/com/access/accessonandroid/FacialRecog/FacialRecogRunnable.java
+++ app/src/main/java/com/access/accessonandroid/FacialRecog/FacialRecogRunnable.java
@@ -4,7 +4,7 @@ import com.amazonaws.services.rekognition.model.Image;
 
 /**
  * You shouldn't have to understand what this class does
- * Created by suzey on 10/26/2017.
+ * Created by Zeyang Su on 10/26/2017.
  */
 
 public class FacialRecogRunnable implements Runnable {
